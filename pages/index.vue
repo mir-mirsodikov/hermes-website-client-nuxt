08a2c84cc93cb5f79480c8a8217ebdd46aba8f4d
Mirmukhammad Mirsodikov <49703214+mir-mirsodikov@users.noreply.github.com>
2022-12-14 15:39:38 -0600

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Hello, World!</h1>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas, dolorem? Omnis quis, ex nam sed, minima nisi
+    tempora incidunt quod earum excepturi veniam, aliquam voluptas reiciendis et quibusdam magnam repellendus.
+  </p>
+  <p>{{ num }}</p>
+  <button
+    class="bg-violet-500 p-3 text-center text-white"
+    @click="add"
+  >
+    Add
+  </button>
+  <p>{{ msg }}</p>
+</template>
+
+<script setup lang="ts">
+const msg = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+const num = ref(3);
+
+const add = () => {
+  num.value++;
+};
+</script>
